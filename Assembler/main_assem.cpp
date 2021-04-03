f75ccd1f79da235a0c0d0a60e28ca3dadef59e6a
Daniil Talashkevich <talashkevich.da@phystech.edu>
2021-04-03 15:16:41 +0300

    Add new stack_cpp_library

--- Assembler/main_assem.cpp
+++ Assembler/main_assem.cpp
@@ -1,11 +1,29 @@
 #include "assem.h"
 
-int main()	//int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     printf("Assembling in progress..\n");
 
-    FILE* user_code = fopen("user_code[for user].asm", "rb");	// fopen(argv[1], "rb");
-    assert(user_code);
+    FILE* user_code = nullptr;
+
+    if(argc == 1)
+    {
+        user_code = fopen("user_code[for_user].asm", "rb");	// fopen(argv[1], "rb");
+        assert(user_code && "can't open user_code");
+    }
+    else if(argc == 2)
+    {
+        user_code = fopen(argv[argc - 1], "rb");	// fopen(argv[1], "rb");
+        assert(user_code && "can't open argv[argc - 1]");
+    }
+    else
+    {
+        printf("Too much arguments.\nExit..");
+        for(int i = 0; i < argc; i++)
+            printf("[%d].%s\n", i + 1, argv[i]);
+        fclose(user_code);
+        return 2;
+    }
 
     Text text_c(user_code);
 
